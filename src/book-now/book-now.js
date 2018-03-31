402dcf290ac0e620b79d194abb169077cd73f362
borkjt9 <borkjt9@gmail.com>
2018-03-31 15:15:11 -0400

    Added loading spinner to form

--- src/book-now/book-now.js
+++ src/book-now/book-now.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './book-now.scss';
 import MapContainer from './google-maps/google-api';
 import GoogleMapWrapper from './google-map/google-map';
 import TextView from './Util/TextView.js';
 import { run, ruleRunner } from './Validation/ruleRunner.js'
 import { required, mustMatch, minLength, validEmail } from './Validation/rules.js';
 import $ from 'jquery';
+import swal from 'sweetalert2';
+import Spinner from 'react-spinkit';
+import './book-now.scss';
 
 const fieldValidations = [
   ruleRunner("name", "Name", required),
@@ -24,6 +26,7 @@ class BookNowForm extends Component {
       email: '',
       date: '',
       message: '',
+      loading: false,
       showErrors: false,
       validationErrors: { },
     };
@@ -69,8 +72,23 @@ class BookNowForm extends Component {
     return this.state.validationErrors[field] || "";
   }
 
+  resetForm() {
+    this.formattedDate = '';
+    this.setState({
+      name: '',
+      email: '',
+      date: '',
+      message: '',
+      loading: false,
+      showErrors: false,
+      validationErrors: { },
+    });
+  }
+
   submitOrder() {
     const { name, email, message } = this.state;
+    this.setState({loading: true})
+
     axios({
       method: "post",
       url: "https://nm3sutzmu2.execute-api.us-east-1.amazonaws.com/devel/sendOrder",
@@ -83,11 +101,21 @@ class BookNowForm extends Component {
         }
       }
     }).then((response)=>{
-        if (response.data.msg === 'success'){
-            alert("Message Sent.");
-            this.resetForm()
-        }else if(response.data.msg === 'fail'){
-            alert("Message failed to send.")
+      this.resetForm()
+
+        if (response.data.status === 'success'){
+
+            swal(
+              'Great!',
+              'A representative from Loaded Barrels BBQ will contact you about your order.',
+              'success'
+            );
+        } else {
+          swal(
+            'Error',
+            'Something went wrong. Please check your internet connection and try again.',
+            'error'
+          );
         }
     })
   }
@@ -101,11 +129,29 @@ class BookNowForm extends Component {
     }
   }
 
+
+  renderLoader() {
+    return (
+      <div className="loading-wrapper">
+        <Spinner className="loading-wrapper__spinner" name="circle" color="#941A00"/>
+      </div>
+    )
+  }
+
+  renderLoader() {
+    return (
+      <div className="loading-wrapper">
+        <Spinner className="loading-wrapper__spinner" name="circle" color="white" fadeIn="none"/>
+      </div>
+    )
+  }
+
   render() {
     const { name, email, date, message } = this.state;
     const messagePlaceholder = "I am hosting a signing party for my newest work and I am in a bind for food!!! I can’t find good bbq in the North. I need your help!!!!";
     return (
         <div className="book-now__form">
+
           <h5 className="book-now__form__text">NAME</h5>
           <TextView placeholder="Tom Jefferson" type="text" showError={this.state.showErrors}
                 text={this.state.name} onChange={this.onChange('name')}
@@ -120,13 +166,18 @@ class BookNowForm extends Component {
                 errorText={this.errorFor("date")} />
           <h5 className="book-now__form__text">MESSAGE</h5>
           <textarea className="is-message" placeholder={messagePlaceholder} showError={this.state.showErrors}
-                text={this.state.message} onChange={this.onChange('message')} />
-          <button className="book-now__form__submit" onClick={this.handleSubmit}><h4>SUBMIT</h4></button>
+                value={this.state.message} onChange={this.onChange('message')} />
+          <button className="book-now__form__submit" onClick={this.handleSubmit}>
+            {this.state.loading ? this.renderLoader() : <h4>SUBMIT</h4>}
+          </button>
 
         </div>
     )
   }
 };
+
+
+
 
 function BookNow() {
   const bookDesc = 'Loaded Barrels BBQ caters to corporate and private events in the tri-state area.';
